5225d736c80352934ba2a83a1bedd9f5c126169a
raja-ar <developer.raaja@gmail.com>
2015-09-20 12:12:46 +0530

    RaceAr  launch 1.0.4  Removed Annoying Ads While Game Runs ..

--- app/src/main/java/com/RaceAr/RaceAr.java
+++ app/src/main/java/com/RaceAr/RaceAr.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2015 Azmeer Raja
+ *  Copyright 2015 Azmeer Raja
  *
- *      Licensed under the Apache License, Version 2.0 (the "License");
- *      you may not use this file except in compliance with the License.
- *      You may obtain a copy of the License at
+ *       Licensed under the Apache License, Version 2.0 (the "License");
+ *       you may not use this file except in compliance with the License.
+ *       You may obtain a copy of the License at
  *
- *          http://www.apache.org/licenses/LICENSE-2.0
+ *           http://www.apache.org/licenses/LICENSE-2.0
  *
- *      Unless required by applicable law or agreed to in writing, software
- *      distributed under the License is distributed on an "AS IS" BASIS,
- *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *      See the License for the specific language governing permissions and
- *      limitations under the License.
+ *       Unless required by applicable law or agreed to in writing, software
+ *       distributed under the License is distributed on an "AS IS" BASIS,
+ *       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *       See the License for the specific language governing permissions and
+ *       limitations under the License.
  */
 
 package com.RaceAr;
@@ -20,16 +20,24 @@ package com.RaceAr;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.RaceAr.widget.NetworkUtil;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class RaceAr extends Activity {
+
     /** Called when the activity is first created. */
 	private OpenGLView _View;
-	
+	private InterstitialAd mInterstitialAd;
+
 	private Boolean _FocusChangeFalseSeen = false;
 	private Boolean _Resume = false;
     @Override
@@ -97,6 +105,86 @@ public class RaceAr extends Activity {
         }
         return super.onKeyUp(keyCode, event);
     }
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch(keyCode){
+			case KeyEvent.KEYCODE_BACK:
+
+				if (NetworkUtil.getConnectivityStatus(RaceAr.this) == 0) {
+
+				} else {
+					launchAd();
+					loadAd();
+					finish();
+				}
+
+				return true;
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
+
+
+	private void loadAd() {
+		AdRequest adRequest = new AdRequest.Builder()
+				.build();
+
+		mInterstitialAd.loadAd(adRequest);
+	}
+
+	private void showAd() {
+		if (mInterstitialAd.isLoaded()) {
+			mInterstitialAd.show();
+		} else {
+			Log.d("Ad", "Not Loaded");
+		}
+	}
+
+	private void launchAd() {
+		mInterstitialAd = new InterstitialAd(this);
+		mInterstitialAd.setAdUnitId(getString(R.string.race_ad));
+
+		mInterstitialAd.setAdListener(new AdListener() {
+			@Override
+			public void onAdLoaded() {
+				super.onAdLoaded();
+				showAd();
+			}
+
+			@Override
+			public void onAdFailedToLoad(int errorCode) {
+				super.onAdFailedToLoad(errorCode);
+				String msg = String.format("onFaildtoLoad (%s)", getErrorReson(errorCode));
+			}
+
+			@Override
+			public void onAdClosed() {
+				super.onAdClosed();
+			}
+		});
+
+	}
+	private String getErrorReson(int errcode) {
+		String errReason = "";
+		switch (errcode) {
+			case AdRequest.ERROR_CODE_INTERNAL_ERROR:
+				errReason = "Internal Error";
+				break;
+			case AdRequest.ERROR_CODE_INVALID_REQUEST:
+				errReason = "invalid request";
+				break;
+			case AdRequest.ERROR_CODE_NETWORK_ERROR:
+				errReason = "Network err";
+				break;
+			case AdRequest.ERROR_CODE_NO_FILL:
+				errReason = "No Fill";
+				break;
+		}
+		return errReason;
+	}
+
+
 
 
 }
