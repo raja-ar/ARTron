cc81ade131ccae0553be7fcd121c677853fff77a
raja-ar <developer.raaja@gmail.com>
2015-10-26 10:02:21 +0530

    update2.1.0 removed ads

--- app/build/generated/source/buildConfig/debug/com/RaceAr/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/RaceAr/BuildConfig.java
@@ -24,6 +24,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.RaceAr";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 299;
-  public static final String VERSION_NAME = "2.9.9";
+  public static final int VERSION_CODE = 210;
+  public static final String VERSION_NAME = "2.1.0";
 }
